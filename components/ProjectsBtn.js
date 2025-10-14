7c1c24825306540e52fdd0ef88210fbc66ff83e0
znalfarel <harvestscary@gmail.com>
2025-10-14 14:50:00 +0700

    fix circles button my project in mobile mode

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -9,13 +9,13 @@ import { HiArrowRight } from "react-icons/hi2";
 
 const ProjectsBtn = () => {
   return <div className="mx-auto xl:mx-0 relative z-10">
-    <Link href={'/work'} className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group">
+    <Link href={'/work'} className="relative w-[150px] h-[150px] xl:w-[180px] xl:h-[180px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group">
     <Image 
       src={'/rounded-text.png'} 
       width={141} 
       height={148} 
       alt='' 
-      className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px] "/>
+      className="animate-spin-slow w-full h-full max-w-[125px] max-h-[132px] xl:max-w-[141px] xl:max-h-[148px]"/>
       <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
       </Link>
   </div>;
